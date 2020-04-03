de07810e966a7940f68222670885c27596fb1d9a
sheldonsu28 <sheldonsu28@gmail.com>
2020-04-03 06:29:44 -0400

    Test case

--- algorithm.py
+++ algorithm.py
@@ -7,6 +7,8 @@ def readImagesAndTimes(paths):
     image = []
     for x in paths:
         image.append(cv.imread(x))
+    align = cv.createAlignMTB()
+    align.process(image, image)
     return image
 
 def tone_merge(images, times):
@@ -20,8 +22,8 @@ def tone_map(HDR_object):
     return result
 
 def save_result(HDR_result):
-    result_image = np.clip(HDR_result * 255, 0, 255).astype('uint16')
-    cv.imwrite("Result.jpg", result_image)
+    result_image = np.clip(HDR_result * 255, 0, 255).astype('uint8')
+    cv.imwrite("./Test Images/1/consistent/Result.jpg", result_image)
 
 def graph_camera_response_function(images, times):
     calibration = cv.createCalibrateDebevec()
@@ -29,19 +31,25 @@ def graph_camera_response_function(images, times):
     return crf_debevec
 
 if __name__ == "__main__":
-    time = np.array([1/50, 1/4, 1, 4], dtype=np.float32)
-    paths = ["./Test Images/3/111.jpg",
-             "./Test Images/3/222.jpg",
-             "./Test Images/3/333.jpg",
-             "./Test Images/3/444.jpg"]
+    time = np.array([1.0/1304.0, 1.0/90.0, 1.0/3.0, 3], dtype=np.float32)
+    paths = ["./Test Images/1/consistent/1.jpg",
+             "./Test Images/1/consistent/2.jpg",
+             "./Test Images/1/consistent/3.jpg",
+             "./Test Images/1/consistent/4.jpg"]
     images = readImagesAndTimes(paths)
+    merge_mertens = cv.createMergeMertens()
+    fusion = merge_mertens.process(images)
+    fusion_8bit = np.clip(fusion * 255, 0, 255).astype('uint8')
+    cv.imwrite("./Test Images/1/consistent/fusion_result.png", fusion_8bit)
     hdr_merge = tone_merge(images, time)
     mapping = tone_map(hdr_merge)
     save_result(mapping)
     x_value = [x for x in range(0, 256)]
     vectors = graph_camera_response_function(images, time)
-    plt.plot(x_value, vectors[:, :, 0])
-    plt.plot(x_value, vectors[:, :, 1])
-    plt.plot(x_value, vectors[:, :, 2])
-    plt.savefig('3.png')
+    plt.plot(x_value, vectors[:, :, 0], label='B', color='b')
+    plt.plot(x_value, vectors[:, :, 1], label='G', color='g')
+    plt.plot(x_value, vectors[:, :, 2], label='R', color='r')
+    plt.legend()
+    plt.title("Estimated camera response function")
+    plt.savefig('./Test Images/1/consistent/CRF.png')
     plt.show()
